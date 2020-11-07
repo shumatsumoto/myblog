5ce7ac0c715ea7bbdb699815c6a022a859d15e80
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-11-07 22:43:30 +0900

    modelAdmin 編集

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+	list_display = ('title', 'content',)
+
+admin.site.register(models.Blog, BlogAdmin)
