7ddd6e980c962cc65031132738726292fecac1f6
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-11-07 22:58:25 +0900

    blog list作成

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import BlogList
 
 urlpatterns = [
-  path('', views.hello),
+  path('', BlogList.as_view(), name='blog_list'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Blog
 
 
-def hello(request):
-	return render(request, 'index.html')
+class BlogList(ListView):
+	template_name = 'myapp/templates/blog_list.html'
+	model = Blog
