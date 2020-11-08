d6501ee472135ab61e0b14c5c0f2e7e83c1a07a9
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-11-08 16:54:28 +0900

    DeleteView

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Blog
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 
 class BlogList(ListView):
@@ -21,3 +21,19 @@ class BlogCreate(CreateView):
 
 	def get_success_url(self):
 		return reverse('blog_list')
+
+
+class BlogUpdate(UpdateView):
+	template_name = 'myapp/templates/blog_form.html'
+	model = Blog
+	fields = ['title', 'content']
+
+	def get_success_url(self):
+		return reverse('blog_detail', kwargs={'pk': self.object.pk})
+
+
+class BlogDelete(DeleteView):
+	template_name = 'myapp/templates/blog_confirm_delete.html'
+	model = Blog
+
+	success_url = reverse_lazy('blog_list')
