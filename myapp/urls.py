18b1380fc3c4f3dc111af7bd6cf35ab43914053b
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-11-08 12:39:30 +0900

    blog detail作成

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogList
+from .views import BlogList, BlogDetail
 
 urlpatterns = [
   path('', BlogList.as_view(), name='blog_list'),
+  path('detail/<int:pk>', BlogDetail.as_view(), name='blog_detail'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Blog
 
 
 class BlogList(ListView):
 	template_name = 'myapp/templates/blog_list.html'
+	model = Blog
+
+
+class BlogDetail(DetailView):
+	template_name = 'myapp/templates/blog_detail.html'
 	model = Blog
